c5d8a0c1deff4d5c40ad12c81ccea946219e3a22
linqf019 <123@163.com>
2019-08-15 19:30:48 +0800

    get

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,6 +5,7 @@ public class Client {
     private String age;
     private String id;
     private String pic;
+    private String getPic;
     public static void main(String[] args) {
         //    我们要代理的真实对象
         Subject realSubject = new RealSubject();
